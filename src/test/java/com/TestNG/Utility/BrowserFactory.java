f70ea89a79b385d65fd3df1b7595e97fc560c035
DISHANTB <DISHANTB@users.noreply.github.com>
2019-06-30 23:45:38 +0530

    Folder Structure change

--- src/test/java/com/TestNG/Utility/BrowserFactory.java
+++ src/test/java/com/TestNG/Utility/BrowserFactory.java
@@ -8,7 +8,9 @@ public class BrowserFactory {
 	public WebDriver getDriver(String BrowserName){
 		WebDriver driver = null;
 		if(BrowserName.toLowerCase().equals("chrome")){
-			System.setProperty("webdriver.chrome.driver","C://Users//DISHANT//Downloads//chromedriver_win32//chromedriver.exe");
+			String systemPath = System.getProperty("user.dir");
+			System.out.println(systemPath);
+			System.setProperty("webdriver.chrome.driver",systemPath.trim()+"//Drivers//chromedriver.exe");
 			driver = new ChromeDriver();
 		}else if(BrowserName.toLowerCase().equals("firefox")){
 			driver = new FirefoxDriver();
